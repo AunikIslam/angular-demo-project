7d4f857679c3fce9a31fd2993c10844cfc883895
AunikIslam <47604952+AunikIslam@users.noreply.github.com>
2023-05-26 20:13:58 +0600

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./main/home/home.module').then(m => m.HomeModule)
+  }
+]
 
 @NgModule({
+  declarations: [AppComponent],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: []
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
