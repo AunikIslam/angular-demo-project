699043e277384d4f2c4e3ef8b5b3575a2bb652c2
AunikIslam <47604952+AunikIslam@users.noreply.github.com>
2023-05-27 12:02:35 +0600

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
   constructor() { 
-    window.location.href = 'home';
+    
   }
 
   ngOnInit() {
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { AppModule } from './app/app.module';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
+import 'zone.js/dist/zone';
 
+if(environment.production){
+  enableProdMode();
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
