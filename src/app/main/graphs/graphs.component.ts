497e4959d330826386c697bd6491cd508197a960
AunikIslam <47604952+AunikIslam@users.noreply.github.com>
2023-06-07 16:24:28 +0600

    Graph Details Added

--- src/app/main/graphs/graphs.component.ts
+++ src/app/main/graphs/graphs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api-calls/api-calls-service';
 
 @Component({
   selector: 'app-graphs',
@@ -8,118 +9,117 @@ import { Component, OnInit } from '@angular/core';
 export class GraphsComponent implements OnInit {
 
   chartData: any;
-  constructor() { }
+  totalItems: any [] = [];
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
-    this.chartData = {
-      series: [
-        {
-          name: "Inflation",
-          data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
-        }
-      ],
-      chart: {
-        height: 350,
-        type: "bar"
-      },
-      plotOptions: {
-        bar: {
-          dataLabels: {
-            position: "top" // top, center, bottom
-          }
-        }
-      },
-      dataLabels: {
-        enabled: true,
-        formatter: function(val) {
-          return val + "%";
-        },
-        offsetY: -20,
-        style: {
-          fontSize: "12px",
-          colors: ["#304758"]
-        }
-      },
+    const apiUrl = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.20716/dados/ultimos/10?formato=json';
+    this.apiService.getData(apiUrl).subscribe(pResponse => {
+      const dataList: any[] = [];
+      const categories: any[] = [];
+      this.totalItems = pResponse;
+      this.totalItems.forEach(pItem => {
+        dataList.push(pItem.valor);
+        categories.push(pItem.data);
 
-      xaxis: {
-        categories: [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "Jun",
-          "Jul",
-          "Aug",
-          "Sep",
-          "Oct",
-          "Nov",
-          "Dec"
+      });
+      this.chartData = {
+        series: [
+          {
+            name: "Value",
+            data: dataList
+          }
         ],
-        position: "top",
-        labels: {
-          offsetY: -18
+        chart: {
+          height: 350,
+          type: "bar"
         },
-        axisBorder: {
-          show: false
-        },
-        axisTicks: {
-          show: false
-        },
-        crosshairs: {
-          fill: {
-            type: "gradient",
-            gradient: {
-              colorFrom: "#D8E3F0",
-              colorTo: "#BED1E6",
-              stops: [0, 100],
-              opacityFrom: 0.4,
-              opacityTo: 0.5
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              position: "top" // top, center, bottom
             }
           }
         },
-        tooltip: {
+        dataLabels: {
           enabled: true,
-          offsetY: -35
-        }
-      },
-      fill: {
-        type: "gradient",
-        gradient: {
-          shade: "light",
-          type: "horizontal",
-          shadeIntensity: 0.25,
-          gradientToColors: undefined,
-          inverseColors: true,
-          opacityFrom: 1,
-          opacityTo: 1,
-          stops: [50, 0, 100, 100]
-        }
-      },
-      yaxis: {
-        axisBorder: {
-          show: false
-        },
-        axisTicks: {
-          show: false
-        },
-        labels: {
-          show: false,
           formatter: function(val) {
             return val + "%";
+          },
+          offsetY: -20,
+          style: {
+            fontSize: "12px",
+            colors: ["#304758"]
+          }
+        },
+  
+        xaxis: {
+          categories: categories,
+          position: "bottom",
+          labels: {
+            offsetY: -5
+          },
+          axisBorder: {
+            show: false
+          },
+          axisTicks: {
+            show: false
+          },
+          crosshairs: {
+            fill: {
+              type: "gradient",
+              gradient: {
+                colorFrom: "#D8E3F0",
+                colorTo: "#BED1E6",
+                stops: [0, 100],
+                opacityFrom: 0.4,
+                opacityTo: 0.5
+              }
+            }
+          },
+          tooltip: {
+            enabled: false,
+            offsetY: -35
+          }
+        },
+        fill: {
+          type: "gradient",
+          gradient: {
+            shade: "light",
+            type: "horizontal",
+            shadeIntensity: 0,
+            gradientToColors: undefined,
+            inverseColors: true,
+            opacityFrom: 1,
+            opacityTo: 1,
+            stops: [50, 0, 100, 100]
+          }
+        },
+        yaxis: {
+          axisBorder: {
+            show: false
+          },
+          axisTicks: {
+            show: false
+          },
+          labels: {
+            show: false,
+            formatter: function(val) {
+              return val + "%";
+            }
+          }
+        },
+        title: {
+          text: "Brazil Central Bank data",
+          floating: 0,
+          offsetY: 330,
+          align: "center",
+          style: {
+            color: "#444"
           }
         }
-      },
-      title: {
-        text: "Monthly Inflation in Argentina, 2002",
-        floating: 0,
-        offsetY: 320,
-        align: "center",
-        style: {
-          color: "#444"
-        }
-      }
-    };
+      };
+    });
   }
 
 }
