747fe572eddf5d9420f29a48bfe4c03cf816415f
AunikIslam <47604952+AunikIslam@users.noreply.github.com>
2023-06-08 12:51:40 +0600

    Radialbar Graph //wip

--- src/app/main/graphs/graphs.component.ts
+++ src/app/main/graphs/graphs.component.ts
@@ -10,9 +10,17 @@ export class GraphsComponent implements OnInit {
 
   chartData: any;
   totalItems: any [] = [];
+
+  radialBarChart: any;
+  radialBarTotalItems: any [] = [];
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.reloadApiOne();
+    this.reloadApiTwo();
+  }
+
+  reloadApiOne(): void {
     const apiUrl = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.20716/dados/ultimos/10?formato=json';
     this.apiService.getData(apiUrl).subscribe(pResponse => {
       const dataList: any[] = [];
@@ -120,6 +128,66 @@ export class GraphsComponent implements OnInit {
         }
       };
     });
+  }
+
+  reloadApiTwo(): void {
+    this.radialBarChart = {
+      series: [76, 67, 61, 90],
+      chart: {
+        height: 190,
+        type: "radialBar"
+      },
+      plotOptions: {
+        radialBar: {
+          offsetY: 0,
+          startAngle: 0,
+          endAngle: 270,
+          hollow: {
+            margin: 5,
+            size: "30%",
+            background: "transparent",
+            image: undefined
+          },
+          dataLabels: {
+            name: {
+              show: false
+            },
+            value: {
+              show: false
+            }
+          }
+        }
+      },
+      colors: ["#1ab7ea", "#0084ff", "#39539E", "#0077B5"],
+      labels: ["Vimeo", "Messenger", "Facebook", "LinkedIn"],
+      legend: {
+        show: true,
+        floating: true,
+        fontSize: "16px",
+        position: "left",
+        offsetX: 50,
+        offsetY: 10,
+        labels: {
+          useSeriesColors: true
+        },
+        formatter: function(seriesName, opts) {
+          return seriesName + ":  " + opts.w.globals.series[opts.seriesIndex];
+        },
+        itemMargin: {
+          horizontal: 3
+        }
+      },
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            legend: {
+              show: false
+            }
+          }
+        }
+      ]
+    };
   }
 
 }
