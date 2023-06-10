8c0dbe14aafad7db91075e38722199c0f7fff3bf
AunikIslam <47604952+AunikIslam@users.noreply.github.com>
2023-06-10 11:23:15 +0600

    Http Reqs Added

--- src/app/main/student/student-service.ts
+++ src/app/main/student/student-service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
 import { catchError, retry } from "rxjs/operators";
@@ -7,8 +7,8 @@ import { Student } from "./student";
 @Injectable({ providedIn: 'root'})
 
 export class StudentService {
-  // // Base url
-  private heroesUrl = 'http://localhost:3000/';
+  
+  private baseUrl = 'http://localhost:3000/';
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
@@ -17,27 +17,32 @@ export class StudentService {
   constructor(private http: HttpClient) {}
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-
-      // Let the app keep running by returning an empty result.
       return of(result as T);
     };
   }
 
-  getStudents (): Observable<Student[]> {
-    return this.http.get<Student[]>(this.heroesUrl)
+  getStudents (data?: any): Observable<Student[]> {
+    return this.http.get<Student[]>(this.baseUrl, {
+      params: new HttpParams({fromObject: data})
+    })
       .pipe(
         catchError(this.handleError('getStudents', []))
       );
   }
 
   addStudent (student: Student): Observable<Student> {
-    return this.http.post<Student>(this.heroesUrl, student, this.httpOptions).pipe(
+    return this.http.post<Student>(this.baseUrl, student, this.httpOptions).pipe(
       catchError(this.handleError<Student>('addStudent'))
     );
+  }
+
+  editStudent (student: Student): Observable<Student> {
+    return this.http.put<Student>(this.baseUrl, student, this.httpOptions).pipe(
+      catchError(this.handleError<Student>('editStudent'))
+    );
+  }
+
+  deleteStudent (): Observable<any> {
+    return this.http.delete(this.baseUrl);
   }
 }
